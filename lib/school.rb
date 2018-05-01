fe64fd026d8403af9c68d997e36b661ca1d68b57
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-01 18:38:33 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -16,7 +16,7 @@ class School
 
   def grade(grade)
     students_in_grade = []
-    @roster.each { |key,value| students_in_grade << value.join(" , ") if key == grade }
+    @roster.each { |key,value| students_in_grade << value.split if key == grade }
     students_in_grade
   end
 
