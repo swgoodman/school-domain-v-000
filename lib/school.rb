eff161220042f861aadb850a2b29c556cfbdf8c3
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-01 18:34:30 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -16,7 +16,7 @@ class School
 
   def grade(grade)
     students_in_grade = []
-    students_in_grade << Hash[grade, @roster[grade]]
+    @roaster.each { |key,value| students_in_grade << value if key == 'grade' }
     binding.pry
     puts "#{students_in_grade}"
   end
