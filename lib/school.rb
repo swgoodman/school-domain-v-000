7b0aa73033c91c0ffee2240b8df98b253878fd01
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-01 17:07:17 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,15 +3,12 @@
 class School
   attr_reader :school
 
-  ROSTER = Hash.new
-
   def initialize(school)
     @school = school
   end
 
   def roster=(roster)
     @roster = {}
-    ROSTER << roster
   end
 
 end
